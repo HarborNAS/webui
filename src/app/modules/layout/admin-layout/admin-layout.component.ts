9f9ad2917717a14c5aa6dbda125bd475151751b4
meetqy <meetqy@icloud.com>
2025-09-27 15:47:16 +0800

    layout

--- src/app/modules/layout/admin-layout/admin-layout.component.ts
+++ src/app/modules/layout/admin-layout/admin-layout.component.ts
@@ -1,10 +1,20 @@
-import { AsyncPipe } from '@angular/common';
-import { AfterViewInit, ChangeDetectionStrategy, Component, computed, OnDestroy, OnInit, QueryList, ViewChildren, inject } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  OnDestroy,
+  OnInit,
+  QueryList,
+  ViewChildren,
+  inject,
+} from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import {
-  MatDrawerMode, MatSidenav, MatSidenavContainer, MatSidenavContent,
+  MatDrawerMode,
+  MatSidenav,
+  MatSidenavContainer,
 } from '@angular/material/sidenav';
-import { MatTooltip } from '@angular/material/tooltip';
 import { RouterOutlet } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Store } from '@ngrx/store';
@@ -14,21 +24,12 @@ import { exploreNasEnterpriseLink } from 'app/constants/explore-nas-enterprise-l
 import { productTypeLabels } from 'app/enums/product-type.enum';
 import { hashMessage } from 'app/helpers/hash-message';
 import { SubMenuItem } from 'app/interfaces/menu-item.interface';
-import { AlertsPanelComponent } from 'app/modules/alerts/components/alerts-panel/alerts-panel.component';
 import { alertPanelClosed } from 'app/modules/alerts/store/alert.actions';
 import { selectIsAlertPanelOpen } from 'app/modules/alerts/store/alert.selectors';
 import { iconMarker } from 'app/modules/ix-icon/icon-marker.util';
-import { IxIconComponent } from 'app/modules/ix-icon/ix-icon.component';
 import { LanguageService } from 'app/modules/language/language.service';
-import { ConsoleFooterComponent } from 'app/modules/layout/console-footer/console-footer.component';
-import { CopyrightLineComponent } from 'app/modules/layout/copyright-line/copyright-line.component';
-import { NavigationComponent } from 'app/modules/layout/navigation/navigation.component';
-import { SecondaryMenuComponent } from 'app/modules/layout/secondary-menu/secondary-menu.component';
 import { SidenavService } from 'app/modules/layout/sidenav.service';
-import { TopbarComponent } from 'app/modules/layout/topbar/topbar.component';
-import { TruenasLogoComponent } from 'app/modules/layout/topbar/truenas-logo/truenas-logo.component';
-import { DefaultPageHeaderComponent } from 'app/modules/page-header/default-page-header/default-page-header.component';
-import { TestDirective } from 'app/modules/test-id/test.directive';
+// import { TopbarComponent } from 'app/modules/layout/topbar/topbar.component';
 import { ThemeService } from 'app/modules/theme/theme.service';
 import { SentryConfigurationService } from 'app/services/errors/sentry-configuration.service';
 import { SessionTimeoutService } from 'app/services/session-timeout.service';
@@ -36,7 +37,10 @@ import { AppState } from 'app/store';
 import { waitForPreferences } from 'app/store/preferences/preferences.selectors';
 import { selectHasConsoleFooter } from 'app/store/system-config/system-config.selectors';
 import {
-  selectCopyrightHtml, selectIsEnterprise, selectProductType, waitForSystemInfo,
+  selectCopyrightHtml,
+  selectIsEnterprise,
+  selectProductType,
+  waitForSystemInfo,
 } from 'app/store/system-info/system-info.selectors';
 
 @UntilDestroy()
@@ -46,23 +50,11 @@ import {
   styleUrls: ['./admin-layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    TestDirective,
     MatSidenavContainer,
-    MatSidenav,
-    IxIconComponent,
-    NavigationComponent,
-    SecondaryMenuComponent,
-    MatTooltip,
-    CopyrightLineComponent,
-    MatSidenavContent,
-    TopbarComponent,
-    DefaultPageHeaderComponent,
+    // DefaultPageHeaderComponent,
     RouterOutlet,
-    ConsoleFooterComponent,
-    AlertsPanelComponent,
-    AsyncPipe,
+    // ConsoleFooterComponent,
     TranslateModule,
-    TruenasLogoComponent,
   ],
 })
 export class AdminLayoutComponent implements OnInit, AfterViewInit, OnDestroy {
@@ -76,14 +68,20 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChildren(MatSidenav) private sideNavs: QueryList<MatSidenav>;
 
   protected readonly iconMarker = iconMarker;
-  readonly hostname$ = this.store$.pipe(waitForSystemInfo, map(({ hostname }) => hostname));
+  readonly hostname$ = this.store$.pipe(
+    waitForSystemInfo,
+    map(({ hostname }) => hostname),
+  );
+
   readonly isAlertPanelOpen$ = this.store$.select(selectIsAlertPanelOpen);
   readonly hasConsoleFooter$ = this.store$.select(selectHasConsoleFooter);
   readonly copyrightHtml = toSignal(this.store$.select(selectCopyrightHtml));
   readonly productType = toSignal(this.store$.select(selectProductType));
   readonly isEnterprise = toSignal(this.store$.select(selectIsEnterprise));
 
-  protected currentMessageHref = computed(() => `${exploreNasEnterpriseLink}?m=${hashMessage(this.productType())}`);
+  protected currentMessageHref = computed(
+    () => `${exploreNasEnterpriseLink}?m=${hashMessage(this.productType())}`,
+  );
 
   get sidenavWidth(): string {
     return this.sidenavService.sidenavWidth;
@@ -128,9 +126,11 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit, OnDestroy {
     this.sessionTimeoutService.start();
     this.themeService.loadTheme$.next('');
     this.sentryService.init();
-    this.store$.pipe(waitForPreferences, untilDestroyed(this)).subscribe((config) => {
-      this.languageService.setLanguage(config.language);
-    });
+    this.store$
+      .pipe(waitForPreferences, untilDestroyed(this))
+      .subscribe((config) => {
+        this.languageService.setLanguage(config.language);
+      });
     this.listenForSidenavChanges();
   }
 
@@ -141,14 +141,18 @@ export class AdminLayoutComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private disableSidenavFocusTrap(): void {
     this.sideNavs?.forEach((sidenav) => {
-      const sidenavWithFocusTrap = sidenav as unknown as { _focusTrap?: { enabled: boolean } };
+      const sidenavWithFocusTrap = sidenav as unknown as {
+        _focusTrap?: { enabled: boolean };
+      };
       if (sidenavWithFocusTrap._focusTrap) {
         sidenavWithFocusTrap._focusTrap.enabled = false;
       }
     });
 
     // Also remove any focus trap anchors from DOM
-    const focusTrapAnchors = document.querySelectorAll('.cdk-focus-trap-anchor');
+    const focusTrapAnchors = document.querySelectorAll(
+      '.cdk-focus-trap-anchor',
+    );
     focusTrapAnchors.forEach((anchor) => anchor.remove());
   }
 
