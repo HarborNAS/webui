9ad2f370ff0fc20fdebefdf13e0f12e171c9275d
meetqy <meetqy@icloud.com>
2025-09-29 17:09:28 +0800

    desktop

--- src/app/pages/desktop/desktop.component.ts
+++ src/app/pages/desktop/desktop.component.ts
@@ -1,18 +1,34 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { UntilDestroy } from '@ngneat/until-destroy';
+import { TranslateModule } from '@ngx-translate/core';
+import { MenuItem, MenuItemType } from 'app/interfaces/menu-item.interface';
 import { IxIconComponent } from 'app/modules/ix-icon/ix-icon.component';
+import { NavigationService } from 'app/services/navigation/navigation.service';
 
 @UntilDestroy()
 @Component({
   selector: 'ix-desktop',
   templateUrl: './desktop.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [IxIconComponent, RouterLink],
+  imports: [IxIconComponent, RouterLink, TranslateModule],
 })
 
 export class DesktopComponent implements OnInit {
   readonly isLoading = false;
+  private navService = inject(NavigationService);
+
+  menuItems = this.navService.menuItems;
+
+  readonly MenuItemType = MenuItemType;
+
+  getItemName(item: MenuItem): string {
+    return `${item.name.replace(' ', '_')}-menu`;
+  }
+
+  getRouterLink(url: string): string[] {
+    return ['/', ...url.split('/')];
+  }
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
