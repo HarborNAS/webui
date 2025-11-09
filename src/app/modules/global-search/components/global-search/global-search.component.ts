cbc24da609d375b76fd535222c1b95b71bc51211
meetqy <meetqy@icloud.com>
2025-11-09 22:04:41 +0800

    delete console

--- src/app/modules/global-search/components/global-search/global-search.component.ts
+++ src/app/modules/global-search/components/global-search/global-search.component.ts
@@ -134,7 +134,6 @@ export class GlobalSearchComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   closeAllBackdrops(): void {
-    console.log('close', '------')
     this.slideIn.closeAll();
     this.sidenavService.closeSecondaryMenu();
     this.dialogService.closeAllDialogs();
