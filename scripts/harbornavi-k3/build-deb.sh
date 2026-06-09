cd5be8baa155a6f7fa489564cac73c52b50877ca
Bean-Harbor <bean.wang@harborinn.com>
2026-06-09 11:25:58 +0800

    fix(k3): proxy gateway turns in WebUI package

--- scripts/harbornavi-k3/build-deb.sh
+++ scripts/harbornavi-k3/build-deb.sh
@@ -78,6 +78,24 @@ server {
         proxy_set_header X-Forwarded-Proto $scheme;
     }
 
+    location = /api/gateway {
+        proxy_pass http://127.0.0.1:8787/api/gateway;
+        proxy_http_version 1.1;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    location /api/gateway/ {
+        proxy_pass http://127.0.0.1:8787/api/gateway/;
+        proxy_http_version 1.1;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
     location = /api/harbor-gate {
         proxy_pass http://127.0.0.1:8787;
         proxy_http_version 1.1;
